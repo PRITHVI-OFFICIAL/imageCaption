ef845a7007237581ecc5e62740b70e6afa7acdff
PRITHVI P K <85571473+PRITHVI-OFFICIAL@users.noreply.github.com>
2025-01-02 17:24:12 +0530

    Update imageCaption.py

--- imageCaption.py
+++ imageCaption.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 # Load the pre-trained YOLOv5 model (small version 'yolov5s')
 torch.hub._validate_not_a_forked_repo=lambda a,b,c: True
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s',trust_repo=True)
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
 # Pydantic model to accept base64 image data
 class ImagePayload(BaseModel):
